5ed33d5d93c353bc12e6684650b883790f9ac3b7
Nikita <Nkmelnikov@hotmail.com>
2017-08-28 20:50:41 +0300

    bug fixing

--- js/app.js
+++ js/app.js
@@ -23,6 +23,8 @@ var rightAreaError = $('.right-area-error');
 var payAgainButton = $('.pay-again');
 var newPaymentAfterError = $('.right-area-error .new-payment');
 
+console.log(getCurrentTime());
+
 firstInput.on('input',function () {
 	var usdVal = $(this).val();
 	secondInput.val((usdVal*0.00023).toFixed(5));
@@ -132,6 +134,7 @@ successButton.click(function () {
 	$('.transaction-hash-container').show();
 	$('.we-accept-block').hide();
 	$('.back-arrow-container').hide();
+	$('.saved-for-text').html('<b>Completed</b> @'+ getCurrentTime());
 });
 
 newPaymentButton.click(function () {
@@ -148,6 +151,7 @@ errorButton.click(function () {
 	rightAreaError.hide();
 	$('.we-accept-block').hide();
 	$('.back-arrow-container').hide();
+	$('.saved-for-text').html('<b>Completed</b> @'+ getCurrentTime());
 });
 //------
 
@@ -170,7 +174,18 @@ $('.terms-cross').click(function () {
 
 
 //-----
+function getCurrentTime(){
+	var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
+		"July", "Aug", "Sept", "Oct", "Nov", "Dec"
+	];
+	var dt = new Date();
+	var time = dt.getHours() + ":" + dt.getMinutes();
+	var month = dt.getMonth();
+	var day = dt.getDate();
 
+	var date = dt.getFullYear() + ' ' + monthNames[month] + ' '+ ((''+day).length<2 ? '0' : '') + day;
+	return time + ', ' + date;
+}
 function isValidEmailAddress(emailAddress) {
 	var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 	return pattern.test(emailAddress);
@@ -198,4 +213,5 @@ function resetState() {
 	secondInput.val('');
 	$('.we-accept-block').show();
 	$('.back-arrow-container').hide();
+	$('.saved-for-text').html('Exchange rate saved for<b>15:46</b>');
 }
